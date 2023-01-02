5b1b8e791e6d54f8867cc0709a9e77a32a8b58df
delmoralcristian <delmoralcristian@gmail.com>
2023-01-02 16:36:41 -0300

    Update readme file

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# super-heros
-Superheros API repo
+# Superhero Management API #
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+### What is this repository for? ###
+
+This application is in charge of superheroes management.
+
+### How do I get set up? ###
+
+* You need to set in your system:
+    * docker
+    * java 11
+
+### How do I run it locally? ###
+
+* _mvn clean install_
+* _mvn spring-boot:run_
+
+### How do I run it locally with Docker? ###
+
+* _mvn clean package -DskipTests=true docker:build_
+* _docker run -d -p 8080:8080 superhero_
+
+### How to use the API ###
+
+* Go to swagger(http://localhost:8080/swagger-ui.html)
+* First of all, you have to log in
+  * Username: mindata
+  * Password: mindata2023
+
+### Who do I talk to? ###
+
+* delmoralcristian@gmail.com
--- src/main/java/mindata/delmoralcristian/superhero/config/security/SecurityConfiguration.java
+++ src/main/java/mindata/delmoralcristian/superhero/config/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
@@ -29,6 +30,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             .authorizeRequests().antMatchers(AUTH_LIST).authenticated()
             .and()
             .httpBasic().authenticationEntryPoint(swaggerAuthenticationEntryPoint())
